7ac911a00ee260556624f4194cc68dee5526c792
josemaria_yañez <102536119+merikorg@users.noreply.github.com>
2024-06-16 18:44:04 -0500

    dfgfdg

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,5 @@
 
-import {CreateMLCEngine} from "https://esm.run/@mlc-ai/web-llm";
-
-const SELECTED_MODEL = 'gemma-2b-it-q4f32_1-MLC';
-
-const engine = await CreateMLCEngine(
-    SELECTED_MODEL,
-    {
-        initProgressCallback: (info)=>{
-            console.log('initProgress', info);
-        }
-    }
-) 
+import { CreateWebWorkerMLCEngine } from "https://esm.run/@mlc-ai/web-llm";
 
 const $ = el => document.querySelector(el)
 
@@ -20,25 +9,69 @@ const $template = $('#message-template')
 const $messages = $('ul')
 const $container = $('main')
 const $button = $('button')
+const $info = $('small')
 
-$form.addEventListener('submit', event=>{
+let messages = [];
+
+const SELECTED_MODEL = 'gemma-2b-it-q4f32_1-MLC';
+
+const engine = await CreateWebWorkerMLCEngine(
+    new Worker('./assets/js/worker.js', { type: 'module' }),
+    SELECTED_MODEL,
+    {
+        initProgressCallback: (info) => {
+            $info.textContent = `${info.text}%`;
+            if (info.progress === 1) {
+                $button.removeAttribute('disabled')
+            }
+        }
+    }
+)
+
+$form.addEventListener('submit', async (event) => {
     event.preventDefault()
     const messageText = $input.value.trim()
 
-    if(messageText !=''){
+    if (messageText != '') {
         $input.value = ''
     }
 
     addMessage(messageText, 'user')
-    $button.setAttribute('disabled',true)
+    $button.setAttribute('disabled', true)
 
-    setTimeout(()=>{
-        addMessage('hola','bot')
-        $button.removeAttribute('disabled')
-    },2000)
+    const userMessage = {
+        role: 'user',
+        content: messageText
+    }
+
+    messages.push(userMessage)
+
+
+    const chunks = await engine.chat.completions.create({
+        messages,
+        stream: true
+    })
+
+    let replay = ""
+
+    const $botMessage = addMessage("", 'bot')
+
+    for await (const chunk of chunks) {
+        const choice = chunk.choices[0]
+        const content = choice?.delta?.content ?? ""
+        replay += content
+        $botMessage.textContent = replay
+    }
+
+    messages.push({
+        role: 'assistant',
+        content: replay
+    })
+    $container.scrollTop = $container.scrollHeight
+    $button.removeAttribute('disabled')
 })
 
-function addMessage(text, sender){
+function addMessage(text, sender) {
     const cloneTemplate = $template.content.cloneNode(true)
     const $newMessage = cloneTemplate.querySelector('.message')
 
@@ -46,9 +79,11 @@ function addMessage(text, sender){
     const $text = $newMessage.querySelector('p')
 
     $text.textContent = text;
-    $who.textContent = sender === 'bot' ? 'IA':'Tú';
+    $who.textContent = sender === 'bot' ? 'IA' : 'Tú';
     $newMessage.classList.add(sender)
 
     $messages.appendChild($newMessage)
     $container.scrollTop = $container.scrollHeight
+
+    return $text
 }
